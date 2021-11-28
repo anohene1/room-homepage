27d96e29593ae7cb742c83e6c7c1322911753579
anohene1 <anoheneisaac@gmail.co,m>
2021-11-28 17:30:22 +0000

    mobile version completed

--- index.js
+++ index.js
@@ -5,6 +5,26 @@ const btnRight = document.querySelector(".slider__btn--right");
 let currentSlide = 0;
 const maxSlide = slides.length;
 
+const hamburger = document.querySelector(".hamburger");
+const hamburgerIcon = document.querySelector(".hamburger__icon");
+const modalBg = document.querySelector(".modal-background");
+const body = document.querySelector("body");
+const navlist = document.querySelector(".navlist");
+
+hamburger.addEventListener("click", function () {
+  modalBg.classList.toggle("hidden");
+
+  if (modalBg.classList.contains("hidden")) {
+    hamburgerIcon.src = "images/icon-hamburger.svg";
+    body.classList.remove("modal-open");
+    navlist.classList.remove("mobile");
+  } else {
+    hamburgerIcon.src = "images/icon-close.svg";
+    body.classList.add("modal-open");
+    navlist.classList.add("mobile");
+  }
+});
+
 const goToSlide = function (s) {
   slides.forEach(function (slide, index) {
     slide.style.transform = `translateX(${(index - s) * 100}%)`;
